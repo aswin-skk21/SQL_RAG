bc62922cdc03acfcab467a6dec43bbd293ce115e
skad204 <skad204@d204.ipsd.net>
2025-07-10 09:15:52 -0500

    rag pipeline

--- document_parsing.py
+++ document_parsing.py
@@ -1,18 +1,16 @@
+import document_parsing
+import vector_db_store
 import os
-import chromadb
 from dotenv import load_dotenv
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAI
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.document_loaders import JSONLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 load_dotenv()
-   
-def main():
-    loadDocs()
-    
-    
+
 def loadDocs():
     loader = DirectoryLoader("./JSON", glob="**/*.jsonl", loader_cls=JSONLoader, loader_kwargs={
     "jq_schema" : ".",
@@ -30,17 +28,5 @@ def textChunking(docs):
         add_start_index = True
     )
     chunks = text_splitter.split_documents(docs)
-    vector_embedding(chunks)
-    
-def vector_embedding(chunks):
-    api_key = os.getenv("GOOGLE_API_KEY")
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)
-    vector_store = Chroma.from_documents(
-    documents=chunks,
-    embedding=embeddings,
-    collection_name="SQL_RAG",
-    persist_directory="./chroma_langchain_db"
-)
+    vector_db_store.vector_embedding(chunks)
 
-if __name__ == "__main__":
-    main()
